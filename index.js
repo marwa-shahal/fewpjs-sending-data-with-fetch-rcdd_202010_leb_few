6c06933a3433bbf64f00c557ed9e7b7e075435fc
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-06 08:23:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,26 @@
 
 function submitData(name,email){
   
+  let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+// method: "POST" is missing from the object below
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  
 }
